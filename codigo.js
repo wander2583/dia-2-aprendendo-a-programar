8bcde681d91ec432b5aaaffe87e03f86b3b76c21
Wandelei Pereira <wanderlei2583@gmail.com>
2022-08-23 21:24:22 -0300

    Codigos adicionados pela explicação do professor V2

---
+++ codigo.js
@@ -0,0 +1,6 @@
+let contaDoRestaurante = 1000
+let parcelaGorjeta = 0.1
+let gorjetaDoGarcon = contaDoRestaurante * parcelaGorjeta
+
+console.log("A gorjeta do Garçon é igual a:")
+console.log(gorjetaDoGarcon)
